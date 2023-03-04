3fd0c44f711655432f7080ed680954fba71b5faf
João Pinto <lamego.pinto@gmail.com>
2023-03-04 19:08:50 +0100

    Change from multiprocessing.pool.Pool to concurrent.futures.ThreadPool #1

--- chttp/fetch.py
+++ chttp/fetch.py
@@ -1,24 +1,32 @@
+import concurrent.futures
 from dataclasses import dataclass
-from multiprocessing.pool import ThreadPool
-from os import cpu_count
 
 import requests
+
+# https://docs.python.org/3/library/concurrent.futures.html#concurrent.futures.ThreadPoolExecutor
 
 
 @dataclass
 class Fetcher:
     urls: list[str]  # List of urls to be downloaded
-
-    def _start_thread_pool(self):
-        cpus = cpu_count()
-        self._thread_pool = ThreadPool(cpus - 1).imap_unordered(
-            self.fetch_url, self.urls
-        )
+    max_workers: int | None = None
 
     def get_all(self):
-        self._start_thread_pool()
-        for result in self._thread_pool:
-            yield result
+        with concurrent.futures.ThreadPoolExecutor(
+            max_workers=self.max_workers
+        ) as executor:
+            # Start the load operations and mark each future with its URL
+            future_to_url = {
+                executor.submit(self.fetch_url, url): url for url in self.urls
+            }
+            for future in concurrent.futures.as_completed(future_to_url):
+                url = future_to_url[future]
+                try:
+                    result = future.result()
+                except Exception as exc:
+                    print("%r generated an exception: %s" % (url, exc))
+                else:
+                    yield result
 
     def fetch_url(self, url):
         return requests.get(url)
--- tests/test_fetch.py
+++ tests/test_fetch.py
@@ -5,5 +5,6 @@ def test_downlotad():
     test_packages = ["flask", "fastapi"]
     test_urls = [f"https://pypi.org/pypi/{package}/json" for package in test_packages]
     packages = Fetcher(test_urls)
+    print()
     for p in packages.get_all():
         print(p, p.url)
